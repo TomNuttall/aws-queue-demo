f527cb97f09c2f5ee93c7b179f449c9eed0ced16
Tom <tsnuttall@gmail.com>
2024-04-16 20:19:32 +0100

    dashboard: Add github logo

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
         },
       }
     : {}),
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'happy-dom',
