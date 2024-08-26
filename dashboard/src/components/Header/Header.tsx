2fc1f1a066140c60fe1bb8e74921b6fbe3857fad
Tom <tsnuttall@gmail.com>
2024-08-26 11:19:34 +0100

    dashboard: Header tweaks

--- dashboard/src/components/Header/Header.tsx
+++ dashboard/src/components/Header/Header.tsx
@@ -9,15 +9,15 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title, repo }) => {
   return (
-    <div className="header">
-      <div className="header__title">{title}</div>
+    <header className="header">
+      <span className="header__title">{title}</span>
       <div className="header__links">
         <ThemeToggle />
         <a href={repo}>
           <GitHubLogo title="GitHub Project" />
         </a>
       </div>
-    </div>
+    </header>
   )
 }
 
